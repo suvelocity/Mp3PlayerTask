fcb95be0cda7e39e25a84907d9306b273b56fe57
oridavidi100 <oridavidi100@gmail.com>
2021-09-08 15:32:28 +0300

    playlist duration function
    checking the duration of each song id and calculate the sum of them together

--- index.js
+++ index.js
@@ -172,12 +172,22 @@ function editPlaylist(playlistId, songId) {
 
 
 function playlistDuration(id) {
+  let sum=0;
   let list;
   for (let x=0;x<player.playlists.length;x++){
-      if(player.playlists[x].id===id){
-        list= player.playlists[x]
+    if(player.playlists[x].id===id){
+      list= player.playlists[x]
+    }
+  }
+  for (let i=0;i<list.songs.length;i++){
+    for (let j=0;j<player.songs.length;j++){
+      if (list.songs[i]===player.songs[j].id){
+       sum +=player.songs[j].duration;
+      continue;
       }
-}
+    }
+  }
+  return sum
 }
 
 function searchByQuery(query) {
