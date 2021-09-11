82b38ec53ebf4189027cdb4d70a5ddd79670e8b2
ronco1812 <rcohen1812@gmail.com>
2021-09-11 12:20:55 +0300

    added the remove song

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const player = {
     { id: 5, name: 'Israeli', songs: [4, 5] },
   ],
   playSong(song) {
-    console.log("Playing "+song.title+ " from " +song.album+" by "+song.artist+"|"+durationConvert(song.duration)+".");
+    console.log("Playing "+song.title+ " from " +song.album+" by "+song.artist+" | "+durationConvert(song.duration)+".");
   },
 }
 function durationConvert(duration) // converts duration value to mm/ss
@@ -76,19 +76,62 @@ function durationConvert(duration) // converts duration value to mm/ss
       durationArr.unshift(0);
       return durationArr.join("");
 }
+function GetsongById(id) //return song object by id
+{
+  let songObj= player.songs.find(x=> x["id"]===id);
+  return songObj;
+}
+function GetSongIndexById(id) //get index of song in songs array
+{
+  let songIndex= player.songs.indexOf(GetsongById(id));
+  return songIndex;
+}
+ function GetsongfromplaylistBysongId(id) //return playlist songs object by id- not used!
+ {
+   let songObj= player.playlists.find(x=>x["songs"].find(d=> d===id)===id);
+   return songObj;
+ }
+
+  function FilterfromPlaylistByID(id) //filters the array from the song - not used!
+  {
+     let songObj= GetsongfromplaylistBysongId(id);
+     let indexPlaySong=songObj["songs"].filter(x=> x!=id);
+     return indexPlaySong;
+  }
 
 function playSong(id) {
-
-  let songObj= player.songs.find(x=> x["id"]===id);
-  if(songObj===undefined) // if id is not found in the player an error will be thrown
   {
-    throw "Not a Valid ID"
+    let songObj= GetsongById(id);
+    if(songObj===undefined) // if id is not found in the player an error will be thrown
+    {
+      throw "Not a Valid ID"
+    }
+    return player.playSong(songObj);
   }
-  return player.playSong(songObj)
 }
 
 function removeSong(id) {
-  // your code here
+  if(GetsongById(id)===undefined)
+  {
+    throw "invalid ID";
+  }
+  else{
+    let songIndex= GetSongIndexById(id); //get index of song in songs array
+    player.songs.splice(songIndex,1); // removed song from songs array
+    // let songObj= GetsongfromplaylistBysongId(id);  //get object of song from playlist by id
+    // let filteredPlayilst= FilterfromPlaylistByID(id); // filtered array of the songs in playlist
+    // songObj["songs"]=filteredPlayilst;
+    for(let i of player.playlists) //filter playlist from songs with id
+      {
+        for(let j=0;j< i.songs.length; j++)
+        {
+          if(i.songs[j]===id){
+            i.songs.splice(j,1);
+          }
+        }
+      }
+    return player;
+  }
 }
 
 function addSong(title, album, artist, duration, id) {
