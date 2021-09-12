840c0ac2631f1c7ad85aee11e78f8b4dc6d74fcc
eitam1400 <eitamshalom@gmail.com>
2021-09-12 03:29:55 +0300

    song player by ID
    created 3 functions. one to play the song using the play.song method and other 2 function  which one finding the index in the list of songs of the song according to his id, and the other do the same but for playlists.

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-const player = {
+const player = { //object of the 1875 mp3 player clone
   songs: [
     {
       id: 1,
-      title: 'Vortex',
-      album: 'Wallflowers',
-      artist: 'Jinjer',
-      duration: 242,
+      title: 'Boys and Girls',
+      album: 'CTV3',
+      artist: 'Jaden',
+      duration: 223
     },
     {
       id: 2,
-      title: 'Vinda',
-      album: 'Godtfolk',
-      artist: 'Songleikr',
-      duration: 160,
+      title: 'Dejanbem',
+      album: 'Haitian Boy Kodak',
+      artist: 'Kodak Black',
+      duration: 214
     },
     {
       id: 7,
-      title: 'Shiroyama',
-      album: 'The Last Stand',
-      artist: 'Sabaton',
-      duration: 213,
+      title: 'Versatile 3',
+      album: 'Before The Album',
+      artist: 'Kodak Black',
+      duration: 334
     },
     {
       id: 3,
-      title: 'Thunderstruck',
-      album: 'The Razors Edge',
-      artist: 'AC/DC',
-      duration: 292,
+      title: 'Gnarly',
+      album: 'Dying To Live',
+      artist: 'Kodak Black feat. Lil Pump',
+      duration: 214
     },
     {
       id: 4,
-      title: 'All is One',
-      album: 'All is One',
-      artist: 'Orphaned Land',
-      duration: 270,
+      title: 'Freaky Girl',
+      album: 'I AM YOU',
+      artist: 'YNW Melly',
+      duration: 215
     },
     {
       id: 5,
-      title: 'As a Stone',
-      album: 'Show Us What You Got',
-      artist: 'Full Trunk',
-      duration: 259,
-    },
+      title: 'Betrayal',
+      album: 'Trip At Knight',
+      artist: 'Trippie Redd feat. Drake',
+      duration: 151
+    }
   ],
   playlists: [
-    { id: 1, name: 'Metal', songs: [1, 7, 4] },
-    { id: 5, name: 'Israeli', songs: [4, 5] },
+    { id: 1, name: 'Hip-Hop', songs: [1, 3, 4] },
+    { id: 5, name: 'Rap', songs: [2, 7, 5] },
   ],
   playSong(song) {
-    console.log(/* your code here */)
-  },
+    console.log(`Playing ${song.title} from ${song.album} by ${song.artist} | ${sstomm(song.duration)}.`)
+  }
 }
 
-function playSong(id) {
-  // your code here
+function playSong(id) { //play a given song by its id
+  if(getSongIdIndex(id) === undefined) throw("song id not found in list");
+  player.playSong(player["songs"][getSongIdIndex(id)]); //uses the playSong method to play a specific song on the mp3!
+}
+
+function getSongIdIndex(id){  //find the song index from the list (and returns it if exist!- wehn running i assigned before running it)
+  for(let i=0; i<player["songs"].length; i++)
+  {
+   if(player["songs"][i]["id"] === id) return i;
+  }
+} // if i use throw in this function when i use it in other places it stops the running of both used and using functions cuz throw just stop the wholeee operation of the code cuz it realises an error... so better assign it with an if statement before each run of this function in other functions
+
+function getPlaylistIdIndex(id){  //find the playlist index from the list (and returns it if exist!- wehn running i assigned before running it)
+for(let i=0; i<player["playlists"].length; i++)
+{
+ if(player["playlists"][i]["id"] === id) return i;
+}
 }
 
 function removeSong(id) {
